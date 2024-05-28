0101e6117a6f647c55bcd5021359872d3a438eb3
Mackewinsson P <mackewinsson@gmail.com>
2024-05-28 20:25:40 +0200

    fixing main

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+import { FormComponent } from "@component/FormComponent";
 import createUser from "../actions/users/createUser";
 
 export default async function Home() {
   createUser();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <FormComponent />
+    </main>
   );
 }
