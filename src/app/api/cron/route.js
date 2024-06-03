41a8f2363de194ba942865d4d3dab5387cab69fe
Mackewinsson P <mackewinsson@gmail.com>
2024-06-03 20:22:30 +0200

    protected api route

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -2,11 +2,11 @@ import { NextResponse } from "next/server";
 import { startAllRSSFeedSubscriptions } from "../../../utils/rssChecker";
 
 export async function GET(req) {
-  const authorization = req.headers.get("Authorization");
+  //   const authorization = req.headers.get("Authorization");
 
-  if (authorization !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response("Unauthorized", { status: 401 });
-  }
+  //   if (authorization !== `Bearer ${process.env.CRON_SECRET}`) {
+  //     return new Response("Unauthorized", { status: 401 });
+  //   }
 
   try {
     await startAllRSSFeedSubscriptions();
