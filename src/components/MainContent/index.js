4c27d77ac58b53f29b3720a4560e7eab6053ff24
Mackewinsson P <mackewinsson@gmail.com>
2024-06-13 21:49:31 +0200

    ui

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -2,12 +2,9 @@
 import React from "react";
 import { FormComponent } from "@component/FormComponent";
 import deleteRss from "../../actions/users/rss/deleteRss";
+import RssFeedTable from "@component/RssFeedTable";
 
 export function MainContent({ rssFeed }) {
-  const truncate = (str, n) => {
-    return str.length > n ? str.substring(0, n - 1) + "..." : str;
-  };
-
   const handleModal = async () => {
     document.getElementById("my_modal_1").showModal();
     // await deleteRss(rssFeed.rssUrl);
@@ -18,39 +15,12 @@ export function MainContent({ rssFeed }) {
   };
 
   return (
-    <section className="flex min-h-screen flex-col items-center justify-between p-40 bg">
+    <section className="flex min-h-screen flex-col items-center justify-between p-10 bg">
       {!rssFeed ? (
         <FormComponent />
       ) : (
-        <div className="overflow-x-auto w-full mt-10">
-          <table className="table w-full">
-            <thead>
-              <tr>
-                <th>RSS URL</th>
-                <th>Webhook URL</th>
-                <th>Latest Item Timestamp</th>
-              </tr>
-            </thead>
-            <tbody>
-              {rssFeed ? (
-                <tr>
-                  <td>{truncate(rssFeed.rssUrl, 30)}</td>
-                  <td>{truncate(rssFeed.webhookUrl, 30)}</td>
-                  <td>
-                    {rssFeed.latestItemTimestamp
-                      ? new Date(rssFeed.latestItemTimestamp).toLocaleString()
-                      : "N/A"}
-                  </td>
-                </tr>
-              ) : (
-                <tr>
-                  <td colSpan="4" className="text-center">
-                    No RSS Feeds found
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
+        <div className="w-full">
+          <RssFeedTable rssFeed={rssFeed} handleModal={handleModal} />
           <div className="flex justify-center gap-2 mt-5">
             <button className="btn btn-error" onClick={handleModal}>
               Delete
@@ -60,11 +30,10 @@ export function MainContent({ rssFeed }) {
       )}
       <dialog id="my_modal_1" className="modal">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Are you sure you want to delete this RSS?</p>
+          <h3 className="font-bold text-lg">Confirm Deletion</h3>
+          <p className="py-4">Are you sure you want to delete this RSS feed?</p>
           <div className="modal-action">
             <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
               <div className="flex justify-center gap-2 mt-5">
                 <button className="btn">Cancel</button>
                 <button className="btn btn-error" onClick={handleDeleteRss}>
