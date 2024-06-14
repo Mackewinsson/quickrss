43d0c28afe05dfdd66aacdcbdb53520315291e98
Mackewinsson P <mackewinsson@gmail.com>
2024-06-14 12:23:58 +0200

    fix on strufture

--- src/components/RssFeedTable/index.js
+++ src/components/RssFeedTable/index.js
@@ -6,37 +6,39 @@ const RssFeedTable = ({ rssFeed, handleModal }) => {
   };
 
   return (
-    <div className="overflow-x-auto w-full mt-10 bg-white shadow-md rounded-lg">
+    <>
       <h2 className="text-3xl font-bold text-[#494949] mb-4 text-center relative">
         Your <span className="text-[#6fda44]">quick</span>
         <span className="text-[#494949]">RSS</span> Feeds
         <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 h-1 w-1/2 bg-[#6fda44]"></div>
       </h2>
-      <table className="table w-full">
-        <thead>
-          <tr className="bg-gray-100 text-gray-700 uppercase text-sm leading-normal">
-            <th className="py-3 px-6 text-left">RSS URL</th>
-            <th className="py-3 px-6 text-left">Webhook URL</th>
-            <th className="py-3 px-6 text-left">Latest Item Timestamp</th>
-          </tr>
-        </thead>
-        <tbody className="text-gray-600 text-sm font-light">
-          <tr className="border-b border-gray-200 hover:bg-gray-100">
-            <td className="py-3 px-6 text-left whitespace-nowrap">
-              {truncate(rssFeed.rssUrl, 30)}
-            </td>
-            <td className="py-3 px-6 text-left whitespace-nowrap">
-              {truncate(rssFeed.webhookUrl, 30)}
-            </td>
-            <td className="py-3 px-6 text-left whitespace-nowrap">
-              {rssFeed.latestItemTimestamp
-                ? new Date(rssFeed.latestItemTimestamp).toLocaleString()
-                : "N/A"}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+      <div className="overflow-x-auto w-full mt-10 bg-white shadow-md rounded-lg">
+        <table className="table w-full">
+          <thead>
+            <tr className="bg-gray-100 text-gray-700 uppercase text-sm leading-normal">
+              <th className="py-3 px-6 text-left">RSS URL</th>
+              <th className="py-3 px-6 text-left">Webhook URL</th>
+              <th className="py-3 px-6 text-left">Latest Item Timestamp</th>
+            </tr>
+          </thead>
+          <tbody className="text-gray-600 text-sm font-light">
+            <tr className="border-b border-gray-200 hover:bg-gray-100">
+              <td className="py-3 px-6 text-left whitespace-nowrap">
+                {truncate(rssFeed.rssUrl, 30)}
+              </td>
+              <td className="py-3 px-6 text-left whitespace-nowrap">
+                {truncate(rssFeed.webhookUrl, 30)}
+              </td>
+              <td className="py-3 px-6 text-left whitespace-nowrap">
+                {rssFeed.latestItemTimestamp
+                  ? new Date(rssFeed.latestItemTimestamp).toLocaleString()
+                  : "N/A"}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
