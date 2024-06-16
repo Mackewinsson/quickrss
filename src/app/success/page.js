aca2f4828ca3792a4697beeadd8970d34780b823
Mackewinsson P <mackewinsson@gmail.com>
2024-06-16 16:27:38 +0200

    success added

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -1,40 +1,21 @@
 "use client";
 import { useEffect, useState } from "react";
 import JSConfetti from "js-confetti";
-
-const ConfettiCanvas = () => {
-  useEffect(() => {
-    const jsConfetti = new JSConfetti();
-    const interval = setInterval(() => {
-      jsConfetti.addConfetti();
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return null;
-};
+import Link from "next/link";
 
 export default function Success() {
+  useEffect(() => {
+    const jsConfetti = new JSConfetti();
+    jsConfetti.addConfetti({ confettiRadius: 10 });
+  }, []);
   return (
-    <div className="flex items-center justify-center p-40">
-      <ConfettiCanvas />
-      <div role="alert" className="alert alert-success w-80">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="stroke-current shrink-0 h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-        <span>Your purchase has been confirmed!</span>
-      </div>
+    <div className="flex flex-col items-center min-h-screen bg-white pt-52">
+      <h1 className="text-4xl font-bold text-center">
+        Thank you for your purchase!🎉
+      </h1>
+      <Link href="/" passHref>
+        <button className="mt-6 btn btn-primary">Volver al inicio</button>
+      </Link>
     </div>
   );
 }
